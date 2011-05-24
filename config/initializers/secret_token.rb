119001e695f6f33f1b70600c39a7ff42eadd6855
Andriy Zhupnyk <novahoba@gmail.com>
2011-05-24 13:30:59 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PutTake::Application.config.secret_token = 'bf3a605b84d436336b4c9cf8b276daaaa5f6d946ded2c1095813850c9b4d41716d83141cef4dca1508ab675091135c92abc959306db7b6ff17bc44aaee679942'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+PutTake::Application.config.session_store :cookie_store, :key => '_put_take_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# PutTake::Application.config.session_store :active_record_store
